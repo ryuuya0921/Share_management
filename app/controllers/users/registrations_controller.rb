4474eca5c3bf660d36ef132e296cc9ad9383fee1
ryuuya <ryuuya0921@gmail.com>
2024-10-04 07:58:53 +0900

    add:Guest_user no_deleted

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,11 +1,16 @@
 module Users
   class RegistrationsController < Devise::RegistrationsController
-    before_action :ensure_normal_user, only: :destroy
+    before_action :ensure_normal_user, only: [:destroy, :update]
 
     def ensure_normal_user
       return unless resource.email == 'guest@example.com'
 
-      redirect_to root_path, alert: 'ゲストユーザーは削除できません。'
+      redirect_to root_path, alert: 'ゲストユーザーはこの操作を行うことができません。' and return
+    end
+
+    def update
+      ensure_normal_user
+      super
     end
   end
 end
