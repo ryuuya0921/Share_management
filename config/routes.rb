b3ce675e3fcb279d1218d6213e82b6246137c60e
ryuuya <ryuuya0921@gmail.com>
2024-10-04 08:40:47 +0900

    add:profile_setting

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     registrations: 'users/registrations'
   }
-  
+
+  resources :users, only: [:show] # ユーザー専用ページ用のルートを定義
+
   root 'home#index'
 
   devise_scope :user do
