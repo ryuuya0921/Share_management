4824e2321e62537878c3081e06581a099296e123
ryuuya <ryuuya0921@gmail.com>
2024-10-04 06:58:31 +0900

    add:remember_time_setting

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -166,6 +166,9 @@ Devise.setup do |config|
   # The time the user will be remembered without asking for credentials again.
   # config.remember_for = 2.weeks
 
+  # How long a user's session is remembered
+  config.remember_for = 2.weeks
+
   # Invalidates all the remember me tokens when the user signs out.
   config.expire_all_remember_me_on_sign_out = true
 
