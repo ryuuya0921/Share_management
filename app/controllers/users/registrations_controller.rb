50e90b18e017edcdb12880ce9fd6ad18eaafb985
ryuuya <ryuuya0921@gmail.com>
2024-10-04 09:24:38 +0900

    add:name_setting

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -8,9 +8,18 @@ module Users
       redirect_to root_path, alert: 'ゲストユーザーはこの操作を行うことができません。' and return
     end
 
-    def update
-      ensure_normal_user
-      super
+    protected
+
+    def update_resource(resource, params)
+      if params[:password].blank? && params[:password_confirmation].blank? && params[:current_password].blank?
+        resource.update_without_password(params.except(:password, :password_confirmation, :current_password))
+      else
+        super
+      end
+    end
+
+    def after_update_path_for(resource)
+      user_path(resource)
     end
   end
 end
