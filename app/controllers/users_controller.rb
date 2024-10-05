68b525f0bb8d475d194c32a202e1644c1d1aa8b5
ryuuya <ryuuya0921@gmail.com>
2024-10-06 08:02:56 +0900

    add:profile_update_setting

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,23 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  def edit
+    @user = current_user
+  end
+
+  def update
+    @user = current_user
+    if @user.update(user_params)
+      redirect_to profile_path(@user), notice: 'プロフィールが更新されました。'
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image, :bio, :remove_profile_image)
+  end
 end
