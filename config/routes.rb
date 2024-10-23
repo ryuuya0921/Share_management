ab534fc8f81395ae0b353a8d0f0c6133b5849263
ryuuya <ryuuya0921@gmail.com>
2024-10-24 04:17:27 +0900

    add:route

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,10 @@ Rails.application.routes.draw do
   resources :users, only: [:show, :edit, :update] # ユーザー専用ページ用のルートを定義
 
   root 'home#index'
+
   get 'profiles/:id', to: 'profiles#show', as: 'profile'#新しいプロフィール表示ページ用
+
+  resources :posts
 
   devise_scope :user do
     post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
