5df6d61fba2594725d91c2e3a2e3042168f68b0c
ryuuya <ryuuya0921@gmail.com>
2024-10-24 04:49:52 +0900

    add:new_controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,4 +3,23 @@ class PostsController < ApplicationController
   def index
     @posts = current_user.posts
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = current_user.posts.build(post_params)
+    if @post.save
+      redirect_to @post, notice: '投稿が作成されました。'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body, :category, :image)
+  end
 end
