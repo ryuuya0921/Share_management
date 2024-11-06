ef0a1d1b809d54944125524ecada50eafec65672
ryuuya <ryuuya0921@gmail.com>
2024-11-06 20:14:07 +0900

    update:taime_ja

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,7 @@ module ShareManagement
 
     config.i18n.default_locale = :ja
 
-    config.time_zone = 'Tokyo'
+    config.time_zone = 'Asia/Tokyo'
     config.active_record.default_timezone = :local
 
     # Configuration for the application, engines, and railties goes here.
