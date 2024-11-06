e77f11826b590d658dd265e18358b26ace748b0b
ryuuya <ryuuya0921@gmail.com>
2024-11-06 16:31:18 +0900

    add:comment_post_rb

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,7 @@ class Post < ApplicationRecord
   has_one_attached :image
   paginates_per 9
   acts_as_votable
+  has_many :comments, dependent: :destroy
 
   validates :title, presence: true
   validates :category, presence: true
