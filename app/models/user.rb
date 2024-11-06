2a08c88664434cb8f38e114e8410682411e5d6c8
ryuuya <ryuuya0921@gmail.com>
2024-11-06 20:01:24 +0900

    add:user_rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
 
   has_many :posts, dependent: :destroy # ユーザーが削除されたら投稿も削除される
-  has_many :comments, dependent: :destroy # ユーザーが削除されたらコメントも削除される
+  has_many :plaza_posts, dependent: :destroy # これにより、ユーザーが削除されるとその投稿も削除される
   acts_as_voter
 
   devise :database_authenticatable, :registerable,
