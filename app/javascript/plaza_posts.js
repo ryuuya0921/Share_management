f49e3ae58af3e36feea515d1ed4d4cb57a59587c
ryuuya <ryuuya0921@gmail.com>
2024-11-07 18:01:36 +0900

    add:comment_edit_js

---
+++ app/javascript/plaza_posts.js
@@ -0,0 +1,17 @@
+//= require plaza_posts
+
+function toggleEditForm(commentId) {
+  const displayDiv = document.getElementById(`comment-display-${commentId}`);
+  const editFormDiv = document.getElementById(`comment-edit-form-${commentId}`);
+  
+  // 表示を切り替え
+  if (displayDiv.style.display === "none") {
+    displayDiv.style.display = "block";
+    editFormDiv.style.display = "none";
+  } else {
+    displayDiv.style.display = "none";
+    editFormDiv.style.display = "block";
+  }
+}
+
+window.toggleEditForm = toggleEditForm;
