9b81778159f54205ffefcf7f61c9aaede82dffbc
ryuuya <ryuuya0921@gmail.com>
2024-11-07 17:04:13 +0900

    add:comment_db_rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_11_06_104232) do
+ActiveRecord::Schema[7.0].define(version: 2024_11_07_071217) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -37,6 +37,16 @@ ActiveRecord::Schema[7.0].define(version: 2024_11_06_104232) do
     t.integer "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id", null: false
+    t.integer "plaza_post_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["plaza_post_id"], name: "index_comments_on_plaza_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "plaza_posts", force: :cascade do |t|
@@ -94,6 +104,8 @@ ActiveRecord::Schema[7.0].define(version: 2024_11_06_104232) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "comments", "plaza_posts"
+  add_foreign_key "comments", "users"
   add_foreign_key "plaza_posts", "users"
   add_foreign_key "posts", "users"
 end
