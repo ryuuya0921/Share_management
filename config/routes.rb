6f51312f4f37c965339e503dc8249f8093addf87
ryuuya <ryuuya0921@gmail.com>
2024-11-14 22:18:27 +0900

    add:route

--- config/routes.rb
+++ config/routes.rb
@@ -44,4 +44,12 @@ Rails.application.routes.draw do
       delete 'unlike', to: 'likes#unlike'
     end
   end
+
+  def after_sign_in_path_for(resource)
+    dashboard_path
+  end
+
+  def after_sign_out_path_for(resource_or_scope)
+    root_path
+  end
 end
