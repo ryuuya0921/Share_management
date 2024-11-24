cac6dd1deba4d5d220fa052917f1cb8f9ee4c89e
ryuuya <ryuuya0921@gmail.com>
2024-11-24 22:54:00 +0900

    add:gmail_setting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,6 +86,17 @@ Rails.application.configure do
   # require "syslog/logger"
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new "app-name")
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+  address: 'smtp.gmail.com',
+  port: 587,
+  domain: 'gmail.com',
+  user_name: ENV['SMTP_USER'],
+  password: ENV['SMTP_PASSWORD'],
+  authentication: 'plain',
+  enable_starttls_auto: true
+}
+
   if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
     logger.formatter = config.log_formatter
