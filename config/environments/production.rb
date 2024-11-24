336031bab5593cb391011e935e623ffbc4fc89e8
ryuuya <ryuuya0921@gmail.com>
2024-11-25 00:30:28 +0900

    update:mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,7 +90,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
   address: 'smtp.gmail.com',
   port: 587,
-  domain: 'omoide-toshokan.com',
+  domain: 'gmail.com',
   user_name: ENV['SMTP_USER'],
   password: ENV['SMTP_PASSWORD'],
   authentication: 'plain',
