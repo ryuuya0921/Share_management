8149232c1d1272a8b5fc1c662cc447f4c24c4aaa
ryuuya <ryuuya0921@gmail.com>
2025-01-06 17:23:00 +0900

    update:bookshelves_single quote

--- spec/requests/bookshelves_spec.rb
+++ spec/requests/bookshelves_spec.rb
@@ -1,21 +1,26 @@
 require 'rails_helper'
 
-RSpec.describe "Bookshelves", type: :request do
-  describe "GET /bookshelves" do
-    it "returns 200 OK" do
+RSpec.describe 'Bookshelves', type: :request do
+  describe 'GET /bookshelves' do
+    before do
       # ログインユーザーと他のユーザーを作成
-      current_user = create(:user) # ログイン中のユーザー
-      other_user = create(:user)  # 他のユーザー
-      create(:post, user: other_user, public: true) # 他のユーザーの公開投稿を作成
+      current_user = create(:user)
+      other_user = create(:user)
+      create(:post, user: other_user, public: true)
 
       # ログインをシミュレート
       sign_in current_user
 
       # GETリクエストを送信
       get bookshelves_path
+    end
 
-      # ステータスコードが200であることを確認
+    it 'ステータスコードが200であること' do
       expect(response).to have_http_status(:ok)
+    end
+
+    it 'ページのタイトルが正しいこと' do
+      expect(response.body).to include('思い出の図書館 - みんなの本棚')
     end
   end
 end
