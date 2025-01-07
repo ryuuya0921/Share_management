afaadfb5c314f485ebb45446705303a21b1ebd1e
ryuuya <ryuuya0921@gmail.com>
2025-01-07 16:40:32 +0900

    update:bookshelves_rspec

--- spec/bookshelves/post_detail_spec.rb
+++ spec/bookshelves/post_detail_spec.rb
@@ -1,32 +1,34 @@
-RSpec.describe 'Post Details', type: :request do
+RSpec.describe 'Post Details - Images', type: :request do
+  let(:user) { create(:user) }
+  let(:post) { create(:post, user:, title: 'テスト投稿', body: 'テスト本文') }
+
+  before do
+    sign_in user
+    allow_any_instance_of(User).to receive(:voted_for?).and_return(false)
+  end
+
   describe 'GET /posts/:id' do
-    let(:user) { create(:user) }
-    let(:post) { create(:post, user: user, title: 'テスト投稿', body: 'テスト本文') }
+    context '正常な場合' do
+      before { get post_path(post) }
 
-    before do
-      sign_in user
-      allow_any_instance_of(User).to receive(:voted_for?).and_return(false)
-      get post_path(post)
-    end
+      it 'ステータスコードが200であること' do
+        expect(response).to have_http_status(:ok)
+      end
 
-    it 'ステータスコードが200であること' do
-      expect(response).to have_http_status(:ok)
-    end
-
-    it '投稿のタイトルが表示されていること' do
-      expect(response.body).to include(post.title)
-    end
-
-    it '投稿の本文が表示されていること' do
-      expect(response.body).to include(post.body)
+      it '投稿のタイトルが表示されていること' do
+        expect(response.body).to include(post.title)
+      end
     end
 
     context '画像が添付されている場合' do
-      let(:post_with_image) { create(:post, user: user, image: fixture_file_upload(Rails.root.join('spec/fixtures/files/test_image.png'), 'image/png')) }
-
-      before do
-        get post_path(post_with_image)
+      let(:post_with_image) do
+        create(:post, user:, image: fixture_file_upload(
+          Rails.root.join('spec/fixtures/files/test_image.png'),
+          'image/png'
+        ))
       end
+
+      before { get post_path(post_with_image) }
 
       it '投稿画像が表示されていること' do
         expect(response.body).to include('test_image.png')
@@ -34,15 +36,14 @@ RSpec.describe 'Post Details', type: :request do
     end
 
     context '画像が添付されていない場合' do
+      let(:post_without_image) { create(:post, user:) }
+
+      before { get post_path(post_without_image) }
+
       it 'デフォルト画像が表示されていること' do
         default_image_path = ActionController::Base.helpers.asset_path('book_01_brown.png')
         expect(response.body).to include(default_image_path)
       end
     end
-
-    it '戻るボタンが表示されていること' do
-      expect(response.body).to include('戻る')
-    end
-
   end
 end
