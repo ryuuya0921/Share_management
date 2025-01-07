b1fead723afba43b3d1d2fe36eaef02b96aa120a
ryuuya <ryuuya0921@gmail.com>
2025-01-07 17:03:30 +0900

    add:bookshelves_show_rspec

---
+++ spec/bookshelves/show_detail_spec.rb
@@ -0,0 +1,38 @@
+RSpec.describe 'Bookshelf Page', type: :request do
+  let(:user) { create(:user, name: 'テストユーザー', bio: 'テストの自己紹介') }
+  let(:post_with_image) do
+    create(:post, user:, title: '画像付き投稿', category: 'カテゴリ1', genre: 'ジャンル1', 
+           image: fixture_file_upload(Rails.root.join('spec/fixtures/files/test_image.png'), 'image/png'))
+  end
+  let(:post_without_image) { create(:post, user:, title: '画像なし投稿', category: 'カテゴリ2', genre: 'ジャンル2') }
+
+  before do
+    sign_in user
+    allow_any_instance_of(User).to receive(:voted_for?).and_return(false)
+  end
+
+  describe 'GET /users/:id/bookshelf' do
+    context '正常な場合' do
+      before { get bookshelf_path(user) }
+
+      it 'ステータスコードが200であること' do
+        expect(response).to have_http_status(:ok)
+      end
+
+      it 'ユーザー名が表示されていること' do
+        expect(response.body).to include("#{user.name} さんの本棚")
+      end
+
+      it '自己紹介文が表示されていること' do
+        expect(response.body).to include(user.bio)
+      end
+    end
+
+    context '画像が添付されている投稿がある場合' do
+      before do
+        post_with_image
+        get bookshelf_path(user)
+      end
+    end
+  end
+end
