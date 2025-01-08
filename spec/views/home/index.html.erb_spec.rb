1712e5b14bd1f79ac82ef958a35a10286a3f24ce
ryuuya <ryuuya0921@gmail.com>
2025-01-08 21:19:40 +0900

    add:home_index_rspec

--- spec/views/home/index.html.erb_spec.rb
+++ spec/views/home/index.html.erb_spec.rb
@@ -1,5 +1,27 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# RSpec.describe 'home/index.html.erb', type: :view do
-#   pending "add some examples to (or delete) #{__FILE__}"
-# end
+RSpec.describe 'Home', type: :request do
+  describe 'GET /' do
+    it 'トップページが正しく表示されること' do
+      get root_path
+      expect(response).to have_http_status(:ok)
+      expect(response.body).to include('思い出の図書館')
+      expect(response.body).to include('あなたの心を動かした映画、アニメ、漫画、本を「思い出の図書館」に記録してみませんか？')
+    end
+
+    it '新規登録リンクが正しいこと' do
+      get root_path
+      expect(response.body).to include(new_user_registration_path)
+    end
+
+    it 'ログインリンクが正しいこと' do
+      get root_path
+      expect(response.body).to include(new_user_session_path)
+    end
+
+    it 'ゲストログインリンクが正しいこと' do
+      get root_path
+      expect(response.body).to include(users_guest_sign_in_path)
+    end
+  end
+end
